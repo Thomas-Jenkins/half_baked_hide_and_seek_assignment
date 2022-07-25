5f68a637d52f1135afa4bc57508ccac48e8a50ce
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-07-25 16:56:15 -0700

    add hard rest button

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
+const resetButton = document.getElementById('reset');
 
 const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
@@ -10,6 +11,7 @@ const boulderContainer = document.getElementById('boulder-container');
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
+
 
 // initialize state
 const hidingPlaces = ['tree', 'shed', 'boulder'];
@@ -39,6 +41,8 @@ boulderButton.addEventListener('click', () => {
     
 });
 
+
+
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
     // then increment the guesses
@@ -60,7 +64,10 @@ function handleGuess(correctSpot, userGuess) {
         incorrectGuesses++;
         lossesEl.textContent = incorrectGuesses;
     }
+    //I inspected the element in the example and found that the face was added to the correct spot using the code below. I added it to my code to see how it worked and it functioned. But it wasn't the way that I solved it. I think, in the spirit of learning I should stick with my code for my assignment. 
 
+    // const correctLocation = document.getElementById(`${correctSpot}-container`);
+    // correctLocation.classList.add('face');
 
     if (correctSpot === hidingPlaces[0]) {
         treeContainer.classList.add('face');
@@ -70,3 +77,15 @@ function handleGuess(correctSpot, userGuess) {
         boulderContainer.classList.add('face');
     }
 }
+
+resetButton.addEventListener('click', () => {
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    correctGuesses = 0;
+    totalGuesses = 0;
+    incorrectGuesses = 0;
+    totalEl.textContent = 0;
+    lossesEl.textContent = 0;
+    winsEl.textContent = 0;
+});
